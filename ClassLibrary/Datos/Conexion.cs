8535309b300c0d65b65fb894581403431789eaef
Rodrigo Abreu <rebreu1602@gmail.com>
2026-07-20 22:31:57 -0300

    agregue coneccion a BD

--- ClassLibrary/Datos/Conexion.cs
+++ ClassLibrary/Datos/Conexion.cs
@@ -10,7 +10,10 @@ namespace ClassLibrary.Datos
     public static class Conexion
     {
         private static readonly string cadenaConexion =
-            "Server=(localdb)\\MSSQLLocalDB;Database=SurfDB;Trusted_Connection=True;";
+            "Server=tcp:zephyrdev.database.windows.net,1433;Initial Catalog=SurfDB;" +
+            "Persist Security Info=False;User ID=Zephyr;Password=Surf1234;" +
+            "MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;" +
+            "Connection Timeout=30;";
 
         public static SqlConnection ObtenerConexion()
         {
